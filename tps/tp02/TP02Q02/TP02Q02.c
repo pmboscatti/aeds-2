699c5f318509a3011426f68e9d9022dc8082e747
Pedro Boscatti <110415518+pmboscatti@users.noreply.github.com>
2023-03-24 09:46:33 -0300

    update tp02q02

--- tps/tp02/TP02Q02/TP02Q02.c
+++ tps/tp02/TP02Q02/TP02Q02.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Definição do registro de personagem
+// Definição do registro do personagem
 typedef struct Personagem
 {
     char nome[40];
@@ -42,17 +42,19 @@ void leituraAtributo(char atributo[], char descricaoPersonagem[], int index)
 // Imprimir os resultados
 void imprimirAtributos(Personagem personagem)
 {
-    double peso = personagem.peso;
-
-    // Consertar a exibição para garantir a mesma quantidade de casas decimais solicitadas
-    if (fmod(peso, 1) == 0)
-    {
-        printf(" ## %s ## %d ## %.0lf ## %s ## %s ## %s ## %s ## %s ## %s ## \n", personagem.nome, personagem.altura, personagem.peso, personagem.corDoCabelo, personagem.corDaPele, personagem.corDosOlhos, personagem.anoNascimento, personagem.genero, personagem.homeworld);
-    }
+    printf(" ## %s", personagem.nome);
+    printf(" ## %d", personagem.altura);
+    if (fmod(personagem.peso, 1) == 0)
+        printf(" ## %.0lf", personagem.peso);
     else
-    {
-        printf(" ## %s ## %d ## %.1lf ## %s ## %s ## %s ## %s ## %s ## %s ## \n", personagem.nome, personagem.altura, personagem.peso, personagem.corDoCabelo, personagem.corDaPele, personagem.corDosOlhos, personagem.anoNascimento, personagem.genero, personagem.homeworld);
-    }
+        printf(" ## %.1lf", personagem.peso);
+    printf(" ## %s", personagem.corDoCabelo);
+    printf(" ## %s", personagem.corDaPele);
+    printf(" ## %s", personagem.corDosOlhos);
+    printf(" ## %s", personagem.anoNascimento);
+    printf(" ## %s", personagem.genero);
+    printf(" ## %s", personagem.homeworld);
+    printf(" ## \n");
 }
 
 // Função para testar o fim do arquivo
@@ -68,6 +70,85 @@ bool testaFim(char palavra[])
     return teste;
 }
 
+// Função para estruturar o personagem
+Personagem montaPersonagem(char caminhoArquivo[])
+{
+    FILE *leitura = fopen(caminhoArquivo, "r");
+
+    char descricaoPersonagem[1000];
+
+    fscanf(leitura, " %[^\n]s", descricaoPersonagem);
+
+    Personagem personagem; // Cria a variável struct
+
+    int contador = 0;
+
+    for (int i = 0; i < strlen(descricaoPersonagem); i++)
+    {
+        if (descricaoPersonagem[i] == ':')
+        {
+            char atributo[50];
+            contador++;
+
+            switch (contador)
+            {
+            case 1:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                strcpy(personagem.nome, atributo);
+                break;
+            case 2:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                personagem.altura = atoi(atributo);
+                break;
+            case 3:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                for (int i = 0; i < strlen(atributo); i++)
+                {
+                    if (atributo[i] == ',')
+                    {
+                        atributo[i] = atributo[i - 1];
+                        atributo[i - 1] = '0';
+                    }
+                }
+                personagem.peso = atof(atributo);
+                break;
+            case 4:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                strcpy(personagem.corDoCabelo, atributo);
+                break;
+            case 5:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                strcpy(personagem.corDaPele, atributo);
+                break;
+            case 6:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                strcpy(personagem.corDosOlhos, atributo);
+                break;
+            case 7:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                strcpy(personagem.anoNascimento, atributo);
+                break;
+            case 8:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                strcpy(personagem.genero, atributo);
+                break;
+            case 9:
+                leituraAtributo(atributo, descricaoPersonagem, i + 3);
+                strcpy(personagem.homeworld, atributo);
+
+                i = strlen(descricaoPersonagem); // Encerra os ciclos de repetição desnecessários
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    fclose(leitura);
+
+    return personagem;
+}
+
 int main(void)
 {
     char caminhoArquivo[100];
@@ -78,82 +159,11 @@ int main(void)
     // Testa o fim do arquivo
     while (testaFim(caminhoArquivo) == false)
     {
-        FILE *leitura = fopen(caminhoArquivo, "r");
+        Personagem personagem = montaPersonagem(caminhoArquivo);
 
-        char descricaoPersonagem[1000];
+        imprimirAtributos(personagem); // Imprimir o resultado
 
-        fscanf(leitura, "%[^\n]s", descricaoPersonagem);
-
-        Personagem personagem; // Cria a variável struct
-
-        int contador = 0;
-
-        for (int i = 0; i < strlen(descricaoPersonagem); i++)
-        {
-            if (descricaoPersonagem[i] == ':')
-            {
-                char atributo[50];
-                contador++;
-
-                switch (contador)
-                {
-                case 1:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    strcpy(personagem.nome, atributo);
-                    break;
-                case 2:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    personagem.altura = atoi(atributo);
-                    break;
-                case 3:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    for (int i = 0; i < strlen(atributo); i++)
-                    {
-                        if (atributo[i] == ',')
-                        {
-                            atributo[i] = atributo[i - 1];
-                            atributo[i - 1] = '0';
-                        }
-                    }
-                    personagem.peso = atof(atributo);
-                    break;
-                case 4:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    strcpy(personagem.corDoCabelo, atributo);
-                    break;
-                case 5:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    strcpy(personagem.corDaPele, atributo);
-                    break;
-                case 6:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    strcpy(personagem.corDosOlhos, atributo);
-                    break;
-                case 7:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    strcpy(personagem.anoNascimento, atributo);
-                    break;
-                case 8:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    strcpy(personagem.genero, atributo);
-                    break;
-                case 9:
-                    leituraAtributo(atributo, descricaoPersonagem, i + 3);
-                    strcpy(personagem.homeworld, atributo);
-
-                    i = strlen(descricaoPersonagem); // Encerra os ciclos de repetição desnecessários
-
-                    imprimirAtributos(personagem); // Imprimir os resultados
-                    break;
-                default:
-                    break;
-                }
-            }
-        }
-
-        fclose(leitura);
-
-        scanf("%[^\n]s", caminhoArquivo);
+        scanf(" %[^\n]s", caminhoArquivo);
         getchar();
     }
 
