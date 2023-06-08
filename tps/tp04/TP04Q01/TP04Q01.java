cfe8be05a6010c1a7bbea2e96214e0aa54f61644
Pedro Boscatti <110415518+pmboscatti@users.noreply.github.com>
2023-06-07 22:08:05 -0300

    upload tp04q01

---
+++ tps/tp04/TP04Q01/TP04Q01.java
@@ -0,0 +1,326 @@
+import java.io.*;
+
+class No {
+    public Personagem elemento;
+    public No esq, dir;
+
+    public No(Personagem elemento) {
+        this(elemento, null, null);
+    }
+
+    public No(Personagem elemento, No esq, No dir) {
+        this.elemento = elemento;
+        this.esq = esq;
+        this.dir = dir;
+    }
+}
+
+class ArvoreBinaria {
+    private No raiz;
+    int numeroComparacoes = 0;
+
+    public ArvoreBinaria() {
+        raiz = null;
+    }
+
+    public boolean pesquisar(String nome) {
+        return pesquisar(nome, raiz);
+    }
+
+    private boolean pesquisar(String nome, No i) {
+        boolean resp;
+        if (i == null) {
+            resp = false;
+
+        } else if (nome.equals(i.elemento.getNome())) {
+            resp = true;
+
+        } else if (nome.compareTo(i.elemento.getNome()) < 0) {
+            MyIO.print("esq ");
+            numeroComparacoes++;
+
+            resp = pesquisar(nome, i.esq);
+
+        } else {
+            MyIO.print("dir ");
+            numeroComparacoes++;
+
+            resp = pesquisar(nome, i.dir);
+        }
+        return resp;
+    }
+
+    public void inserir(Personagem x) throws Exception {
+        raiz = inserir(x, raiz);
+    }
+
+    private No inserir(Personagem x, No i) throws Exception {
+        if (i == null) {
+            i = new No(x);
+
+        } else if (x.getNome().compareTo(i.elemento.getNome()) < 0) {
+            numeroComparacoes++;
+            i.esq = inserir(x, i.esq);
+
+        } else if (x.getNome().compareTo(i.elemento.getNome()) > 0) {
+            numeroComparacoes++;
+            i.dir = inserir(x, i.dir);
+
+        } else {
+            throw new Exception("Erro ao inserir!");
+        }
+
+        return i;
+    }
+
+    // Criar arquivo de log
+    public void criarLog(long tempoInicial) {
+        long tempoFinal = System.currentTimeMillis(); // Gravar o tempo do fim da execução
+
+        Arq.openWrite("matricula_arvoreBinaria.txt");
+        Arq.println(
+                "Matricula: 790052\t" + "Numero Comparacoes: " + numeroComparacoes + "\t" + "Tempo Execucao: "
+                        + (tempoFinal - tempoInicial) + "ms");
+        Arq.close();
+    }
+}
+
+class Personagem {
+    private String nome;
+    private int altura;
+    private double peso;
+    private String corDoCabelo;
+    private String corDaPele;
+    private String corDosOlhos;
+    private String anoNascimento;
+    private String genero;
+    private String homeworld;
+
+    public Personagem() {
+    }
+
+    public Personagem(String nome, int altura, double peso, String corDoCabelo, String corDaPele,
+            String corDosOlhos, String anoNascimento, String genero, String homeworld) {
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+        this.corDoCabelo = corDoCabelo;
+        this.corDaPele = corDaPele;
+        this.corDosOlhos = corDosOlhos;
+        this.anoNascimento = anoNascimento;
+        this.genero = genero;
+        this.homeworld = homeworld;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setCorDoCabelo(String corDoCabelo) {
+        this.corDoCabelo = corDoCabelo;
+    }
+
+    public String getCorDoCabelo() {
+        return corDoCabelo;
+    }
+
+    public void setCorDaPele(String corDaPele) {
+        this.corDaPele = corDaPele;
+    }
+
+    public String getCorDaPele() {
+        return corDaPele;
+    }
+
+    public void setCorDosOlhos(String corDosOlhos) {
+        this.corDosOlhos = corDosOlhos;
+    }
+
+    public String getCorDosOlhos() {
+        return corDosOlhos;
+    }
+
+    public String getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(String anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getHomeworld() {
+        return homeworld;
+    }
+
+    public void setHomeworld(String homeworld) {
+        this.homeworld = homeworld;
+    }
+
+    public Personagem clone() {
+        Personagem cloned = new Personagem();
+
+        cloned.nome = this.nome;
+        cloned.altura = this.altura;
+        cloned.peso = this.peso;
+        cloned.corDoCabelo = this.corDoCabelo;
+        cloned.corDaPele = this.corDaPele;
+        cloned.corDosOlhos = this.corDosOlhos;
+        cloned.anoNascimento = this.anoNascimento;
+        cloned.genero = this.genero;
+        cloned.homeworld = this.homeworld;
+
+        return cloned;
+    }
+}
+
+public class TP04Q01 {
+    public static void main(String[] args) throws Exception {
+        MyIO.setCharset("ISO-8859-1");
+        long tempoInicial = System.currentTimeMillis(); // Gravar tempo de início de execução
+
+        ArvoreBinaria arvorePersonagens = new ArvoreBinaria();
+
+        String caminhoArquivo = MyIO.readLine().replaceAll("é", "\u00e9");
+
+        // Testar fim da primeira parte do arquivo
+        while (testaFim(caminhoArquivo) == false) {
+
+            // Montar personagem e inserir na árvore binária
+            arvorePersonagens.inserir(montaPersonagem(caminhoArquivo));
+
+            caminhoArquivo = MyIO.readLine().replaceAll("é", "\u00e9");
+        }
+
+        String nomeConsultaPersonagem = MyIO.readLine().replaceAll("é", "\u00e9");
+
+        // Testar fim da segunda parte do arquivo
+        while (testaFim(nomeConsultaPersonagem) == false) {
+            MyIO.print(nomeConsultaPersonagem + " raiz ");
+
+            if (arvorePersonagens.pesquisar(nomeConsultaPersonagem)) {
+                MyIO.println("SIM");
+            } else {
+                MyIO.println("N�O");
+            }
+
+            nomeConsultaPersonagem = MyIO.readLine().replaceAll("é", "\u00e9");
+        }
+
+        arvorePersonagens.criarLog(tempoInicial); // Passar o tempo de início da execução
+    }
+
+    // Testar fim do arquivo
+    public static boolean testaFim(String palavra) {
+        boolean teste = false;
+
+        if (palavra.equals("FIM")) {
+            teste = true;
+        }
+
+        return teste;
+    }
+
+    // Retornar personagem lido
+    public static Personagem montaPersonagem(String caminhoArquivo) throws Exception {
+        RandomAccessFile leitura = new RandomAccessFile(caminhoArquivo, "r");
+
+        String descricaoPersonagem = leitura.readLine().replaceAll("é", "\u00e9");
+
+        Personagem personagem = new Personagem();
+        int contadorDoisPontos = 0; // Contar separadores
+
+        for (int i = 0; i < descricaoPersonagem.length(); i++) {
+            if (descricaoPersonagem.charAt(i) == ':') {
+                contadorDoisPontos++;
+
+                switch (contadorDoisPontos) {
+                    case 1:
+                        personagem.setNome(leituraAtributo(descricaoPersonagem, i + 3));
+                        break;
+                    case 2:
+                        String atributoInteiro = leituraAtributo(descricaoPersonagem, i + 3);
+                        if (atributoInteiro.equals("unknown")) {
+                            personagem.setAltura(0);
+                        } else {
+                            personagem.setAltura(Integer.parseInt(atributoInteiro));
+                        }
+                        break;
+                    case 3:
+                        String atributoDouble = leituraAtributo(descricaoPersonagem, i + 3).replaceAll(",", ".");
+                        if (atributoDouble.equals("unknown")) {
+                            personagem.setPeso(0);
+                        } else {
+                            personagem.setPeso(Double.parseDouble(atributoDouble));
+                        }
+                        break;
+                    case 4:
+                        personagem.setCorDoCabelo(leituraAtributo(descricaoPersonagem, i + 3));
+                        break;
+                    case 5:
+                        personagem.setCorDaPele(leituraAtributo(descricaoPersonagem, i + 3));
+                        break;
+                    case 6:
+                        personagem.setCorDosOlhos(leituraAtributo(descricaoPersonagem, i + 3));
+                        break;
+                    case 7:
+                        personagem.setAnoNascimento(leituraAtributo(descricaoPersonagem, i + 3));
+                        break;
+                    case 8:
+                        personagem.setGenero(leituraAtributo(descricaoPersonagem, i + 3));
+                        break;
+                    case 9:
+                        personagem.setHomeworld(leituraAtributo(descricaoPersonagem, i + 3));
+                        i = descricaoPersonagem.length(); // Encerra os ciclos de repetição desnecessários
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        leitura.close();
+
+        return personagem;
+    }
+
+    // Retornar valor do atributo do personagem
+    public static String leituraAtributo(String descricaoPersonagem, int index) {
+        String atributo = new String();
+
+        while (descricaoPersonagem.charAt(index) != '\'') {
+            atributo += descricaoPersonagem.charAt(index);
+
+            index++;
+        }
+
+        return atributo;
+    }
+}
